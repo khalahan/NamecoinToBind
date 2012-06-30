edfd3b81114ac735c0fd6bfb238bddd9aba2c1a1
Khalahan <khal@dot-bit.org>
2012-06-30 20:29:14 +0000

    Fix too big serial number on 32bit systems

--- namescan.php
+++ namescan.php
@@ -106,7 +106,7 @@ $template = str_replace('@@DOMAINE@@', 'bit', $template);
 #$template = str_replace('%%serial%%', '1', $template);
 $template = str_replace('%%authns%%', $authoritativeNS[0][0], $template);
 $template = str_replace('%%email%%', 'hostmaster.'.$authoritativeNS[0][0], $template);
-$template = str_replace('%%serial%%', date('YmdHi'), $template);
+$template = str_replace('%%serial%%', time()-1303087979, $template);
 foreach($authoritativeNS as $i => $ns) {
 	$template .= "	  IN NS   ".$authoritativeNS[$i][0].".\n";
 	foreach((array)$authoritativeNS[$i][1] as $ip) {
